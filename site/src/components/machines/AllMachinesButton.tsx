04ee8610189ba75b35ceab6bc4d1c6cdcf29c988
wDeloo <wdelo.hack@gmail.com>
2025-04-06 19:03:43 +0200

    load all machines button lower

--- site/src/components/machines/AllMachinesButton.tsx
+++ site/src/components/machines/AllMachinesButton.tsx
@@ -1,6 +1,6 @@
 export default function AllMachinesButton() {
     return (
-        <a href={`${import.meta.env.BASE_URL}/machines`} className="terminalText text-2xl m-auto px-10 py-6 hover:underline">
+        <a href={`${import.meta.env.BASE_URL}/machines`} className="terminalText text-2xl mx-auto mt-5 px-10 py-6 hover:underline">
             Load all machines...
         </a>
     )
