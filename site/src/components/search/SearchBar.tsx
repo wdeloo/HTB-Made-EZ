ca4a9f5faf9ce3f8c34399580793c01db1feed93
wDeloo <wdelo.hack@gmail.com>
2025-04-06 19:13:11 +0200

    submit when empty text prevented

--- site/src/components/search/SearchBar.tsx
+++ site/src/components/search/SearchBar.tsx
@@ -1,13 +1,22 @@
+import { useRef } from "react"
 import { useSearchParams } from "react-router-dom"
 
 export default function SearchBar() {
     const [params] = useSearchParams()
 
+    const inputRef = useRef<HTMLInputElement>(null)
+
+    function handleSubmit(e: React.FormEvent) {
+        const input = inputRef.current
+        if (!input) return
+        if (!input.value.trim()) e.preventDefault()
+    }
+
     return (
         <search>
-            <form action={`${import.meta.env.BASE_URL}/search`} method="GET">
+            <form onSubmit={handleSubmit} action={`${import.meta.env.BASE_URL}/search`} method="GET">
                 <div className="relative">
-                    <input defaultValue={params.get('q') ?? ""} name="q" type="search" className="outline-none w-full text-xl px-3.5 py-2 rounded bg-neutral-800  placeholder:text-neutral-500 terminalText" placeholder="Enter Machine Name" />
+                    <input ref={inputRef} defaultValue={params.get('q') ?? ""} name="q" type="search" className="outline-none w-full text-xl px-3.5 py-2 rounded bg-neutral-800  placeholder:text-neutral-500 terminalText" placeholder="Enter Machine Name" />
                     <button className="cursor-pointer" type="submit">
                         <img height={27} width={27} className="absolute right-3 top-1/2 translate-y-[-50%]" src={`${import.meta.env.BASE_URL}/img/search.svg`} />
                     </button>
