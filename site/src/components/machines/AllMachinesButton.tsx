9c7feec9893f4c5492bf8d0d9cbb728b35effe80
wDeloo <wdelo.hack@gmail.com>
2025-04-10 19:28:27 +0200

    machines bug link

--- site/src/components/machines/AllMachinesButton.tsx
+++ site/src/components/machines/AllMachinesButton.tsx
@@ -1,7 +1,9 @@
+import { Link } from "react-router-dom";
+
 export default function AllMachinesButton() {
     return (
-        <a href={`${import.meta.env.BASE_URL}/machines`} className="terminalText text-2xl mx-auto mt-5 px-10 py-6 hover:underline">
+        <Link to="/machines" className="terminalText text-2xl mx-auto mt-5 px-10 py-6 hover:underline">
             Load all machines...
-        </a>
+        </Link>
     )
 }
